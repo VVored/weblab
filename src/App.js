20446d4db3ceb451191d2f6712c3e9573b04dc3d
VVored <muchvova-a@mail.ru>
2023-04-10 20:19:48 +0500

    мям мяу

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import vector from './img/Group.svg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container">
+            <div style={{width: '50%'}}>
+                <h1>Разработка сайтов и продвижение</h1>
+                <p>Cоздаем интерфейсы аналитических систем, мобильные приложения и digital-сервисы для государства и бизнеса</p>
+                <div style={{display: 'flex', }}>
+                    <a href="*"><p className="project">Обсудить ваш проект</p></a>
+                </div>
+                <div className="socialNetwork">
+                    <font>Наши соц.сети</font>
+                    <a href="*" style={{backgroundColor: 'blue', color: 'white'}}>Facebook </a>
+                    <a href="*" style={{color: '#696969'}}>VKontakte</a>
+                </div>
+            </div>
+            <div style={{width: '50%'}}>
+                <div className="logo">
+                    <img src={vector} className="vector_element" style={{width: 200, }} alt="((("></img>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
